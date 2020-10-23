c03196f28d97e277f1c55b7ce1f12f5b5486d345
WolverinDEV <git@did.science>
2020-10-23 16:44:01 +0200

    Updated the path

--- build.rs
+++ build.rs
@@ -22,7 +22,7 @@ fn main() {
 
                 let output_path = if cfg!(windows) {
                     /* Causes this issue: https://github.com/mesonbuild/meson/issues/7879 */
-                    std::env::current_dir().unwrap().join("libnice__build")
+                    std::env::current_dir().unwrap().join("__build")
                 } else {
                     PathBuf::from(env::var("OUT_DIR").unwrap()).join("libnice")
                 };
