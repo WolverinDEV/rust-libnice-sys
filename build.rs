82e042b0c2b97eba5e5ae82c7053d4525ad34238
WolverinDEV <git@did.science>
2020-11-09 17:36:46 +0100

    Updated the build script and removed libnice submodule

--- build.rs
+++ build.rs
@@ -4,6 +4,41 @@ extern crate pkg_config;
 use std::env;
 use std::path::PathBuf;
 use std::process::{Command, Stdio};
+use build_utils::build::{LibraryType, BuildStepError, MesonBuild};
+use build_utils::source::BuildSourceGit;
+use build_utils::{BuildStep, BuildResult, Build, execute_build_command};
+use std::hash::{Hasher, Hash};
+
+struct MesonPromote {
+    files: Vec<String>
+}
+
+impl BuildStep for MesonPromote {
+    fn name(&self) -> &str {
+        "meson promote"
+    }
+
+    fn hash(&self, hasher: &mut Box<dyn Hasher>) {
+        self.files.hash(hasher);
+    }
+
+    fn execute(&mut self, build: &Build, _result: &mut BuildResult) -> Result<(), BuildStepError> {
+        let source_dir = build.source().local_directory();
+
+        for file in self.files.iter() {
+            println!("Promoting wrap file {}", file);
+            let mut command = Command::new("meson");
+            command.current_dir(&source_dir)
+                    .arg("wrap")
+                    .arg("promote")
+                    .arg(source_dir.join(file).to_str().unwrap());
+
+            execute_build_command(&mut command, format!("failed to execute promote command for {}", file).as_str())?;
+        }
+
+        Ok(())
+    }
+}
 
 fn main() {
     let mut libnice_include_dirs: Vec<String> = Vec::new();
@@ -20,188 +55,95 @@ fn main() {
             Err(error) => {
                 println!("Pkg-config hasn't found libnice: {}.", error);
 
-                let output_path = if cfg!(windows) {
-                    /* Causes this issue: https://github.com/mesonbuild/meson/issues/7879 */
-                    std::env::current_dir().unwrap().join("out__")
-                } else {
-                    PathBuf::from(env::var("OUT_DIR").unwrap()).join("libnice")
-                };
-                if !output_path.join("lib").join("nice.lib").exists() && !output_path.join("lib").join("libnice.a").exists() {
-                    build_meson(&std::env::current_dir().unwrap().join("libnice"), &output_path, false);
-                }
+                let source = BuildSourceGit::builder("https://github.com/WolverinDEV/libnice.git".to_owned())
+                    .build();
 
-                println!("cargo:rustc-link-lib=dylib=nice");
-                if cfg!(windows) {
-                    println!("cargo:rustc-link-lib=dylib=bcrypt");
-                    println!("cargo:rustc-link-lib=dylib=Iphlpapi");
-                    println!("cargo:rustc-link-search=native={}", output_path.join("lib").to_string_lossy());
-                } else {
-                    let triplet = Command::new("gcc")
-                        .arg("-dumpmachine")
-                        .output().expect("failed to get host triplet")
-                        .stdout;
-                    let triplet = String::from_utf8(triplet).expect("invalid host triplet format");
-                    println!("cargo:rustc-link-search=native={}", output_path.join("lib").join(triplet).to_string_lossy());
-                    println!("cargo:rustc-link-search=native={}", output_path.join("lib").to_string_lossy());
-                }
+                let meson = MesonBuild::builder()
+                    .promote_callback(|source| {
+                        println!("Callback promote for {:?}", source);
+                        vec![
+                            "subprojects/glib-2.64.2/subprojects/zlib.wrap".to_owned(),
+                            "subprojects/glib-2.64.2/subprojects/libffi.wrap".to_owned(),
+                            "subprojects/glib-2.64.2/subprojects/proxy-libintl.wrap".to_owned()
+                        ]
+                    })
+                    .meson_option("gstreamer", "disabled")
+                    .meson_option("tests", "disabled")
+                    .build();
 
-                libnice_include_dirs.push(output_path.join("include").to_string_lossy().into());
-                libnice_include_dirs.push(output_path.join("include").join("glib-2.0").to_string_lossy().into());
-                libnice_include_dirs.push(output_path.join("lib").join("glib-2.0").join("include").to_string_lossy().into());
-            }
-    }
+                let mut build_builder = Build::builder()
+                    .name("libnice")
+                    .source(Box::new(source))
+                    .add_step(Box::new(meson))
+                    .build_path(base_url.clone())
+                    .install_prefix(base_url.join("install_root_nice"))
+                    .remove_build_dir(false);
 
-    /*
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    if !out_path.join("bindings.rs").exists() {
-        let bindings = bindgen::Builder::default()
-            .header_contents(
-                "wrapper.h",
-                "#include <nice/agent.h>
-             #include <nice/interfaces.h>
+                match build_builder.build().expect("failed to generate build").execute() {
+                    Ok(mut result) => {
+                        if cfg!(windows) {
+                            /* required by libnice */
+                            println!("cargo:rustc-link-lib=dylib=bcrypt");
+                            println!("cargo:rustc-link-lib=dylib=Iphlpapi");
+                        }
 
-             #include <stun/stunagent.h>
-             #include <stun/stunmessage.h>
-             #include <stun/constants.h>
-             #include <stun/usages/bind.h>
-             #include <stun/usages/ice.h>
-             #include <stun/usages/turn.h>
-             #include <stun/usages/timer.h>
+                        result.emit_cargo();
 
-             #include <nice/pseudotcp.h>",
-            )
-            // ICE Library
-            .whitelist_function("nice_.+")
-            .whitelist_type("NICE.+")
-            .whitelist_type("_?Nice.+")
-            .whitelist_type("_?TurnServer")
-            // STUN Library
-            .whitelist_function("stun_.+")
-            .whitelist_type("STUN.+")
-            .whitelist_type("TURN.+")
-            .whitelist_type("_?[Ss]tun.+")
-            // contains `va_list` type argument which seems like it might not be handled properly
-            .opaque_type("StunDebugHandler")
-            // Pseudo TCP Socket implementation
-            .whitelist_function("pseudo_tcp_.+")
-            .whitelist_type("_?PseudoTcp.+")
-            // Disable recursive whitelisting, we're using libc, glib-sys, etc.
-            .whitelist_recursively(false)
-            .clang_args(
-                libnice_include_dirs
-                    .iter()
-                    .map(|path| format!("-I{}", path)),
-            )
-            .generate()
-            .expect("Unable to generate bindings");
-        bindings
-            .write_to_file(out_path.join("bindings.rs"))
-            .expect("Couldn't write bindings!");
-    }
-    */
-}
+                        /* TODO: Generate bindings */
+                        /*
+                         let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+                            if !out_path.join("bindings.rs").exists() {
+                                let bindings = bindgen::Builder::default()
+                                    .header_contents(
+                                        "wrapper.h",
+                                        "#include <nice/agent.h>
+                                     #include <nice/interfaces.h>
 
-fn build_meson(source: &PathBuf, output_path: &PathBuf, configs_promoted: bool) {
-    let build_path = if cfg!(windows) {
-        /* Causes this issue: https://github.com/mesonbuild/meson/issues/7879 */
-        std::env::current_dir().unwrap().join("__build")
-    } else {
-        PathBuf::from(env::var("OUT_DIR").unwrap()).join("libnice_build")
-    };
+                                     #include <stun/stunagent.h>
+                                     #include <stun/stunmessage.h>
+                                     #include <stun/constants.h>
+                                     #include <stun/usages/bind.h>
+                                     #include <stun/usages/ice.h>
+                                     #include <stun/usages/turn.h>
+                                     #include <stun/usages/timer.h>
 
-    println!("Building to {}", build_path.to_str().unwrap());
-
-    /* setup the build */
-    {
-        let mut compile = Command::new("meson");
-
-        compile.arg("setup");
-        compile.arg("--prefix");
-        compile.arg(&output_path);
-        let value = env::var("nice_build_type").unwrap_or(String::new());
-        match value.as_str() {
-            "static" => { compile.arg("-Ddefault_library=static"); },
-            "shared" => { compile.arg("-Ddefault_library=shared"); },
-            "" => {  },
-            _ => panic!("Invalid build type: {:?}", value)
-        };
-        compile.arg("-Dgstreamer=disabled");
-        compile.arg("-Dtests=disabled");
-        compile.arg(&build_path);
-        compile.arg(&source);
-        compile.stdout(Stdio::piped());
-
-        let result = compile.spawn().expect("failed to launch meson command")
-            .wait_with_output().expect("failed to wait for the process");
-
-        if ! result.status.success() {
-            let stdout = String::from_utf8_lossy(&result.stdout);
-
-            println!("Failed to execute command:");
-            println!("{}", &stdout);
-            if stdout.find("ERROR: Unknown compiler(s):").is_some() {
-                panic!("Missing any c compiler. If you're under windows,\
-                    ensure the compiler is within the PATH environment variable.");
-            } else if stdout.find("meson wrap promote subprojects").is_some() && !configs_promoted {
-                let glib_subprojects = PathBuf::from("subprojects").join("glib-2.64.2").join("subprojects");
-                for file in [
-                    glib_subprojects.join("zlib.wrap"),
-                    glib_subprojects.join("libffi.wrap"),
-                    glib_subprojects.join("proxy-libintl.wrap"),
-                ].iter() {
-                    println!("Promoting wrap file {}", file.to_str().unwrap());
-                    let succeeded = Command::new("meson")
-                        .current_dir(&source)
-                        .arg("wrap")
-                        .arg("promote")
-                        .arg(file)
-                        .spawn().expect("failed to spawn meson command")
-                        .wait().expect(format!("failed to execute promote command for {}", file.to_str().unwrap()).as_str())
-                        .success();
-                    if ! succeeded {
-                        panic!("Failed to promote {}", file.to_str().unwrap());
+                                     #include <nice/pseudotcp.h>",
+                                    )
+                                    // ICE Library
+                                    .whitelist_function("nice_.+")
+                                    .whitelist_type("NICE.+")
+                                    .whitelist_type("_?Nice.+")
+                                    .whitelist_type("_?TurnServer")
+                                    // STUN Library
+                                    .whitelist_function("stun_.+")
+                                    .whitelist_type("STUN.+")
+                                    .whitelist_type("TURN.+")
+                                    .whitelist_type("_?[Ss]tun.+")
+                                    // contains `va_list` type argument which seems like it might not be handled properly
+                                    .opaque_type("StunDebugHandler")
+                                    // Pseudo TCP Socket implementation
+                                    .whitelist_function("pseudo_tcp_.+")
+                                    .whitelist_type("_?PseudoTcp.+")
+                                    // Disable recursive whitelisting, we're using libc, glib-sys, etc.
+                                    .whitelist_recursively(false)
+                                    .clang_args(
+                                        libnice_include_dirs
+                                            .iter()
+                                            .map(|path| format!("-I{}", path)),
+                                    )
+                                    .generate()
+                                    .expect("Unable to generate bindings");
+                                bindings
+                                    .write_to_file(out_path.join("bindings.rs"))
+                                    .expect("Couldn't write bindings!");
+                            }
+                          */
+                    },
+                    Err(error) => {
+                        error.pretty_format();
+                        panic!("failed to execute libnice build");
                     }
                 }
-
-                build_meson(source, output_path, true);
-                return;
             }
-
-            panic!("Unknown compiler error");
-        }
-    }
-
-    /* build the build */
-    {
-        let mut compile = Command::new("meson");
-        compile.arg("compile");
-        compile.arg("-C");
-        compile.arg(&build_path);
-
-        let success = compile.spawn()
-            .expect("failed to spawn meson command")
-            .wait().expect("failed to wait for the meson command")
-            .success();
-
-        if ! success {
-            panic!("Failed to compile libnice.");
-        }
-    }
-
-    /* install libnice */
-    {
-        let mut compile = Command::new("meson");
-        compile.arg("install");
-        compile.arg("-C");
-        compile.arg(&build_path);
-
-        let success = compile.spawn()
-            .expect("failed to spawn meson command")
-            .wait().expect("failed to wait for the meson command")
-            .success();
-
-        if ! success {
-            panic!("Failed to install libnice.");
-        }
     }
 }
